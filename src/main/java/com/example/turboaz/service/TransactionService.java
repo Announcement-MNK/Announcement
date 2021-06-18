2c78acba8bf8debe5c896a876db00c47c8981de6
kamaltaghiyev <83601296+kamaltaghiyev@users.noreply.github.com>
2021-06-18 19:23:03 +0400

    AN-75 resolved comments : add to Transaction service's method createTransaction field userId and changed getAllTransaction method's return type to List<TransactionDto>

--- src/main/java/com/example/turboaz/service/TransactionService.java
+++ src/main/java/com/example/turboaz/service/TransactionService.java
@@ -3,12 +3,10 @@ package com.example.turboaz.service;
 import com.example.turboaz.dtos.TransactionDto;
 import com.example.turboaz.enums.TransactionPurpose;
 import com.example.turboaz.enums.TransactionType;
-import com.example.turboaz.models.Transaction;
-
 import java.util.List;
 
 public interface TransactionService {
-    TransactionDto createTransaction(long listingId, TransactionPurpose purpose, TransactionType type, double amount);
-    List<Transaction> getAllTransactions();
+    TransactionDto createTransaction(long userId, long listingId, TransactionPurpose purpose, TransactionType type, double amount);
+    List<TransactionDto> getAllTransactions();
     void softDelete(long transactionId);
 }
