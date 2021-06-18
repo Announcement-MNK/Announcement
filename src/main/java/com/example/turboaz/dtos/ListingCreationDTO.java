1a193dec616251225f5d7eb8ee7c390c9e86b059
kamaltaghiyev <83601296+kamaltaghiyev@users.noreply.github.com>
2021-06-18 17:23:28 +0400

    AN-70 create post,get and list dtos of listing

---
+++ src/main/java/com/example/turboaz/dtos/ListingCreationDTO.java
@@ -0,0 +1,31 @@
+package com.example.turboaz.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ListingCreationDTO {
+    private Long makeId;
+    private Long modelId;
+    private int year;
+    private int price;
+    private int mileage;
+    private String fuelType;
+    private String bodyType;
+    private String color;
+    private Long cityId;
+    private String gearBox;
+    private boolean auto_pay;
+    private boolean creditOption;
+    private boolean barterOption;
+    private boolean leaseOption;
+    private boolean cashOption;
+    private String description;
+    private String type;
+    private String thymbnailUrl;
+    private List<Integer> carSpecIds;
+}
