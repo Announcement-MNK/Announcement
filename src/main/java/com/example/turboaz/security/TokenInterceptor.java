fecfb8caee13a1138d937c950ba8dd4976cf361a
kamaltaghiyev <83601296+kamaltaghiyev@users.noreply.github.com>
2021-06-20 13:52:14 +0400

    AN-86 resolve comments: use UserDto instead of new model UserData

--- src/main/java/com/example/turboaz/security/TokenInterceptor.java
+++ src/main/java/com/example/turboaz/security/TokenInterceptor.java
@@ -1,6 +1,6 @@
 package com.example.turboaz.security;
 
-import com.example.turboaz.dtos.UserData;
+import com.example.turboaz.dtos.UserDto;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Component;
@@ -18,19 +18,14 @@ public class TokenInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception{
         String auth = request.getHeader("Authorization");
         if(auth != null){
-            UserData user = new UserData();
+            UserDto user = new UserDto();
             String[] chunks = auth.split("\\.");
             Base64.Decoder decoder = Base64.getDecoder();
             String data = new String(decoder.decode(chunks[1]));
             JsonNode payload = new ObjectMapper().readValue(data,JsonNode.class);
             user.setUsername(payload.get("preferred_name").textValue());
-            user.setName(payload.get("name").textValue());
+            user.setFullName(payload.get("name").textValue());
             user.setPhone(payload.get("phone_number").textValue());
-            user.setEmail(payload.get("email").textValue());
-            user.setRegistrationTime(
-                    LocalDateTime.ofEpochSecond(payload.get("created_date").longValue(),
-                            0,
-                            ZoneOffset.ofHours(4)));
             request.setAttribute("user",user);
         }
         return true;
