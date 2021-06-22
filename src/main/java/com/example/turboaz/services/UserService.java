ad67eef228fa9501adf8b0bdc02708efe451950a
kamaltaghiyev <83601296+kamaltaghiyev@users.noreply.github.com>
2021-06-22 13:41:07 +0400

    fix login function fields and return type

--- src/main/java/com/example/turboaz/services/UserService.java
+++ src/main/java/com/example/turboaz/services/UserService.java
@@ -1,23 +1,19 @@
 package com.example.turboaz.services;
 
-import com.example.turboaz.dtos.RegisterPostDto;
-import com.example.turboaz.dtos.RegisterResponseDto;
-import com.example.turboaz.dtos.UserDto;
+import com.example.turboaz.dtos.*;
 
 public interface UserService {
     /**
      * Login via keycloak
-     * @param email
-     * @param password
+     * @param user
      * @return
      */
-    UserDto login(String email, String password);
+    LoginResponseDto login(LoginPostDto user);
 
     /**
      * Register new user via keycloak
      * @param user
-     * @param password
      * @return
      */
-    RegisterResponseDto register(RegisterPostDto user, String password);
+    RegisterResponseDto register(RegisterPostDto user);
 }
