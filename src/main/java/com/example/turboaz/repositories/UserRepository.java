a45e3bc59eedb5a737dc1550e613c995f2b8db64
kamaltaghiyev <83601296+kamaltaghiyev@users.noreply.github.com>
2021-06-22 05:47:43 +0400

    add recalculate user balance after transactions logic and update user balance method in user repository

--- src/main/java/com/example/turboaz/repositories/UserRepository.java
+++ src/main/java/com/example/turboaz/repositories/UserRepository.java
@@ -1,8 +1,13 @@
 package com.example.turboaz.repositories;
 
 import com.example.turboaz.models.User;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
     User findUserByUsername(String username);
+    @Modifying
+    @Query("UPDATE User u set u.balance = ?2 where u.id = ?1")
+    void updateUserBalance(long id,double balance);
 }
